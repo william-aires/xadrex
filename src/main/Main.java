4f898bdb11b2b737f985b87c72ac734271eee5e1
william aires <williamaires52@gmail.com>
2022-06-29 01:40:13 -0300

    Adicionado class posicao

--- src/main/Main.java
+++ src/main/Main.java
@@ -1,8 +1,11 @@
 package main;
 
+import model_entidades.Posicao;
+
 public class Main {
     public static void main(String[]args) {
-
+        Posicao posicao = new Posicao(3, 5);
+        System.out.println(posicao);
     }
     
 }
--- src/model_entidades/Posicao.java
+++ src/model_entidades/Posicao.java
@@ -1,4 +1,45 @@
 package model_entidades;
 public class Posicao {
+    private int linha;
+    private int coluna;
+    
+    public Posicao(int linha, int coluna) {
+        this.linha = linha;
+        this.coluna = coluna;
+    }
+
+    /**
+     * @return int return the linha
+     */
+    public int getLinha() {
+        return linha;
+    }
+
+    /**
+     * @param linha the linha to set
+     */
+    public void setLinha(int linha) {
+        this.linha = linha;
+    }
+
+    /**
+     * @return int return the coluna
+     */
+    public int getColuna() {
+        return coluna;
+    }
+
+    /**
+     * @param coluna the coluna to set
+     */
+    public void setColuna(int coluna) {
+        this.coluna = coluna;
+    }
+
+    @Override
+    public String toString() {
+        return linha + ", " + coluna;
+    }
+   
 
 }
